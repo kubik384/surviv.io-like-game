a520b6d7d62e67acd922e085d3901b74481b65c5
Borlen <kubik961@seznam.cz>
2019-09-17 20:20:55 +0200

    Update

---
+++ Javascript/server_main.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var formidable = require('../../../node_js/node_modules/npm/node_modules/formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+	if (req.url == '/fileupload') {
+		var form = new formidable.IncomingForm();
+		form.parse(req, function (err, fields, files) {
+			console.log(files);
+			/*
+			var oldpath = files.filetoupload.path;
+			var newpath = 'E:/github_projects/surviv.io-clone/uploads/' + files.filetoupload.name;
+			fs.rename(oldpath, newpath, function (err) {
+			if (err) throw err;
+				res.write('File uploaded and moved!');
+				res.end();
+			});
+			*/
+		});
+	} else {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+		res.write('<input type="file" name="filetoupload"><br>');
+		res.write('<input type="submit">');
+		res.write('</form>');
+		return res.end();
+	}
+}).listen(8080);
