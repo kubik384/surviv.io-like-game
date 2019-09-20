919db4915673cb67d79f092a6c544cbc78e4cd2f
Borlen <kubik961@seznam.cz>
2019-09-20 11:18:48 +0200

    Added node.js modules

--- Javascript/server_main.js
+++ Javascript/server_main.js
@@ -1,21 +1,40 @@
 var http = require('http');
-var formidable = require('../../../node_js/node_modules/npm/node_modules/formidable');
+var path = require('path');
+var express = require('express');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});// Starts the server.
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
+
+
+
+
+
+/* Tutorial from w3 code
+
+var http = require('http');
+var formidable = require('formidable');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
 	if (req.url == '/fileupload') {
 		var form = new formidable.IncomingForm();
 		form.parse(req, function (err, fields, files) {
-			console.log(files);
-			/*
 			var oldpath = files.filetoupload.path;
-			var newpath = 'E:/github_projects/surviv.io-clone/uploads/' + files.filetoupload.name;
+			var newpath = 'C:/Users/Honza/' + files.filetoupload.name;
 			fs.rename(oldpath, newpath, function (err) {
 			if (err) throw err;
 				res.write('File uploaded and moved!');
 				res.end();
 			});
-			*/
 		});
 	} else {
 		res.writeHead(200, {'Content-Type': 'text/html'});
@@ -26,3 +45,4 @@ http.createServer(function (req, res) {
 		return res.end();
 	}
 }).listen(8080);
+*/
