5522ff676274c116752a5acf203341786e05e5b8
Borlen <kubik961@seznam.cz>
2019-09-21 14:51:13 +0200

    Functional server code

--- server_main.js
+++ server_main.js
@@ -1,15 +1,16 @@
+var express = require('express');
 var http = require('http');
 var path = require('path');
-var express = require('express');
 var socketIO = require('socket.io');
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 app.set('port', 5000);
-app.use('/static', express.static(__dirname + '/game'));// Routing
+app.use('/game', express.static(__dirname + '/game'));// Routing
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
+
 // Starts the server.
 server.listen(5000, function() {
   console.log('Starting server on port 5000');
@@ -17,38 +18,6 @@ server.listen(5000, function() {
 // Add the WebSocket handlers
 io.on('connection', function(socket) {
 });
-
 setInterval(function() {
 	io.sockets.emit('message', 'hi!');
 }, 1000);
-
-
-
-/* Tutorial from w3 code
-
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-	if (req.url == '/fileupload') {
-		var form = new formidable.IncomingForm();
-		form.parse(req, function (err, fields, files) {
-			var oldpath = files.filetoupload.path;
-			var newpath = 'C:/Users/Honza/' + files.filetoupload.name;
-			fs.rename(oldpath, newpath, function (err) {
-			if (err) throw err;
-				res.write('File uploaded and moved!');
-				res.end();
-			});
-		});
-	} else {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-		res.write('<input type="file" name="filetoupload"><br>');
-		res.write('<input type="submit">');
-		res.write('</form>');
-		return res.end();
-	}
-}).listen(8080);
-*/
