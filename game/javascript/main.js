3d5ba0994c38f316c27a112d198a40fe63a5750d
Borlen <kubik961@seznam.cz>
2019-09-21 14:37:19 +0200

    Update

--- game/javascript/main.js
+++ game/javascript/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var socket = io();
 var game_area;
 var selectedCanvas;
 
@@ -9,22 +10,25 @@ function startGame() {
 }
 
 // --- Events listeners --- //
+socket.on('message', function(data) {
+	console.log(data);
+});
 window.addEventListener('keydown', function(e) {
 	selectedCanvas.keyDown(e);
-})
+});
 window.addEventListener('keyup', function(e) {
 	selectedCanvas.keyUp(e);
-})
+});
 window.addEventListener('mousedown', function(e) {
 	if (e.button == 0) {
 		selectedCanvas.lMouseDown(e);
 	}
-})
+});
 window.addEventListener('mouseup', function(e) {
 	if (e.button == 0) {
 		selectedCanvas.lMouseUp(e);
 	}
-})
+});
 window.addEventListener('mousemove', function(e) {
 	selectedCanvas.mouseMove(e);
-})
+});
--- server_main.js
+++ server_main.js
@@ -6,15 +6,21 @@ var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 app.set('port', 5000);
-app.use('/static', express.static(__dirname + '/static'));// Routing
+app.use('/static', express.static(__dirname + '/game'));// Routing
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
-});// Starts the server.
+});
+// Starts the server.
 server.listen(5000, function() {
   console.log('Starting server on port 5000');
 });
+// Add the WebSocket handlers
+io.on('connection', function(socket) {
+});
 
-
+setInterval(function() {
+	io.sockets.emit('message', 'hi!');
+}, 1000);
 
 
 
